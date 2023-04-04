4564de9cdbbd8ca20d2cf2398b6fc88aa6740012
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-04 17:47:17 +0200

    Resolve linters

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -2,46 +2,15 @@
 
 def decode_char(char)
   decoder = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z',
-    '-----' => '0',
-    '.----' => '1',
-    '..---' => '2',
-    '...--' => '3',
-    '....-' => '4',
-    '.....' => '5',
-    '-....' => '6',
-    '--...' => '7',
-    '---..' => '8',
-    '----.' => '9',
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '.----' => '1',
+    '..---' => '2', '...--' => '3', '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7',
+    '---..' => '8', '----.' => '9', '-----' => '0'
   }
 
   decoder[char]
-  
 end
 
 puts decode_char('-...')
@@ -49,8 +18,8 @@ puts decode_char('-...')
 
 # Decode Morse code words
 def decode_word(word)
-  characters = word.split(' ')
-  characters.map { |char| char = decode_char(char) }.join
+  characters = word.split
+  characters.map { |char| decode_char(char) }.join
 end
 
 puts decode_word('.- -... -.-. -.. .')
@@ -59,7 +28,7 @@ puts decode_word('.- -... -.-. -.. .')
 # Decode a full sentence of words
 def decode_sentence(sentence)
   words = sentence.split('  ')
-  words.map { |word| word = decode_word(word) }.join(' ')
+  words.map { |word| decode_word(word) }.join(' ')
 end
 
 puts decode_sentence('.-- .... .- - ...  ..- .--.')
